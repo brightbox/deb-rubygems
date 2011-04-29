89ee9163df9c2ae48c7f5f878aa895077da832a2
Daigo Moriwaki <daigo@debian.org>
2011-04-29 17:50:21 +0900

    Tests will be skipped without the minitest library installed, which Debian does not have for Ruby 1.8.

--- debian/ruby-tests.rb
+++ debian/ruby-tests.rb
@@ -3,4 +3,12 @@ EXC_TESTS = [
   'test/rubygems/functional.rb',
   'test/rubygems/insure_session.rb',
 ]
+
+begin
+  require 'rubygems'
+  require 'minitest/autorun'
+rescue LoadError
+  $stderr.puts "WARNING: Skipped tests due to lack of dependency libraries."
+  exit
+end
 (Dir['{spec,test}/**/test_*.rb'] - EXC_TESTS).each { |f| require f }
